a9a414bcb7be2414e02c036dc83cd9500e26d72e
Joel Charles <joel.charles91@gmail.com>
2026-04-03 14:07:14 +0200

    fix: fix deadlock with srvx

--- packages/fastify/src/index.ts
+++ packages/fastify/src/index.ts
@@ -1,11 +1,24 @@
 import { type App, apply } from "@universal-middleware/fastify";
+import { toFetchHandler as _toFetchHandler } from "srvx/node";
 import vikeMiddleware from "vike/universal-middleware";
 
 export * from "@universal-middleware/fastify";
-export { toFetchHandler } from "srvx/node";
 
 type EnhancedMiddlewareFastify = Parameters<typeof apply>[1][number];
 
 export default function vike(app: App, middlewares: EnhancedMiddlewareFastify[] = []) {
   return apply(app, [...middlewares, vikeMiddleware]);
 }
+
+export const toFetchHandler: typeof _toFetchHandler = (handler) => {
+  const fetchHandler = (request: Request) =>
+    // biome-ignore lint/suspicious/noExplicitAny: ok
+    _toFetchHandler((req: any, res: any) => {
+      // Cache used by universal-middleware
+      req[Symbol.for("unRequest")] = request;
+      return handler(req, res);
+    })(request);
+  return (request) => {
+    return fetchHandler(request);
+  };
+};
